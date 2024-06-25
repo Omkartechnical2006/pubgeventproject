32475eafea66c3109192f6c0e8ac6786d34d0c27
Omkartechnical2006 <114220878+Omkartechnical2006@users.noreply.github.com>
2024-06-25 21:02:42 +0530

    Delete public directory

--- public/script/faq.js
+++
@@ -1,7 +0,0 @@
-const ansshow =(e)=>{
-    let questions = document.getElementsByClassName("answer");
-    for(let question of questions){
-        question.style.display="none";
-    }
-    e.nextElementSibling.style.display="block";
-   }
